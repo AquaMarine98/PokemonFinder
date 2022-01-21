aeaf0244437d34ff21c83597438f77519aa898e9
Aqua <vadell57@gmail.com>
2022-01-21 16:38:55 -0300

    Añadi un archivo html simple y un js con la logica para buscar pokemons y mostrar su nombre y su sprite

---
+++ pokemonFinder.js
@@ -0,0 +1,18 @@
+let button = document.getElementById("btn");
+button.addEventListener("click", () => {
+    let xhttp = new XMLHttpRequest();
+    let pokemonName = document.getElementById("caja").value;
+    let p = document.getElementById("show-pokemon-name");
+    let img = document.getElementById("img");
+
+    xhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let dataPokemon = JSON.parse(this.responseText);
+            img.setAttribute("src", dataPokemon.sprites.front_default);
+            p.textContent = dataPokemon.name;
+        }
+    }
+})
