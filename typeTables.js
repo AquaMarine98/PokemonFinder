4232b72e847e193544a2bc5b5ff0b22684550e70
Aqua <vadell57@gmail.com>
2022-01-26 19:16:37 -0300

    agregue un js con la informacion de que color tiene que ser el tipo de pokemon

---
+++ typeTables.js
@@ -0,0 +1,56 @@
+function changeBackground(type) {
+    if(type.textContent == "normal"){
+        type.style.background = 'rgb(185, 152, 152)';
+    }
+    if(type.textContent == "fire"){
+        type.style.background = 'rgb(255, 115, 0)';
+    }
+    if(type.textContent == "water"){
+        type.style.background = 'rgb(84, 133, 246)';
+    }
+    if(type.textContent == "grass"){
+        type.style.background = 'rgb(124, 189, 28)';
+    }
+    if(type.textContent == "electric"){
+        type.style.background = 'rgb(255, 255, 0)';
+    }
+    if(type.textContent == "ice"){
+        type.style.background = 'rgb(183, 235, 255)';
+    }
+    if(type.textContent == "fighting"){
+        type.style.background = 'rgb(160, 2, 2)';
+    }
+    if(type.textContent == "poison"){
+        type.style.background = 'rgb(128, 0, 128)';
+    }
+    if(type.textContent == "ground"){
+        type.style.background = 'rgb(210, 180, 140)';
+    }
+    if(type.textContent == "flying"){
+        type.style.background = 'rgb(91, 91, 200)';
+    }
+    if(type.textContent == "psychic"){
+        type.style.background = 'rgb(216, 112, 147)';
+    }
+    if(type.textContent == "bug"){
+        type.style.background = 'rgb(88, 131, 68)';
+    }
+    if(type.textContent == "rock"){
+        type.style.background = 'rgb(119, 66, 22)';
+    }
+    if(type.textContent == "ghost"){
+        type.style.background = 'rgb(32, 0, 68)';
+    }
+    if(type.textContent == "dragon"){
+        type.style.background = 'rgb(15, 71, 124)';
+    }
+    if(type.textContent == "dark"){
+        type.style.background = 'rgb(41, 41, 41)';
+    }
+    if(type.textContent == "steel"){
+        type.style.background = 'rgb(192, 192, 192)';
+    }
+    if(type.textContent == "fairy"){
+        type.style.background = 'rgb(238, 130, 238)';
+    }
+ }
